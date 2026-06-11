0dc07e1ea0debc6f93549717cec2ecd201611733
Chris Fournier <chris.d.fournier@gmail.com>
2026-06-11 10:27:14 -0400

    Drift correction -4 (-2 still right, jumping to -4)

--- picar_server.py
+++ picar_server.py
@@ -365,7 +365,7 @@ def drive():
     duration = max(0, min(5.0, float(data.get("duration", 0))))
     continuous = data.get("continuous", False)
 
-    px.set_dir_servo_angle(angle - 2)  # -2 drift correction for right drift
+    px.set_dir_servo_angle(angle - 4)  # -4 drift correction for right drift
     if direction == "forward":
         px.forward(speed)
     else:
